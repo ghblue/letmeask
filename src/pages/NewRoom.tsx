9c3ebb27fef8ca7d348b2e051f3d4ef4a2005a4d
ghblue <ghresilva@.inf.ufpel.edu.br>
2021-06-23 19:11:31 -0300

    feat/new-pages

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
+
+import { Button } from '../components/Button'
+// import { useAuth } from '../hooks/useAuth'
+
 
 import '../styles/auth.scss'
-import { Button } from '../components/Button'
 
 export function NewRoom(){
+
+    // const { user } = useAuth()
+
     return(
         <div id="page-auth">
             <aside>
@@ -26,7 +33,7 @@ export function NewRoom(){
                         </Button>
                     </form>
                     <p>
-                        Quer entrar em uma sala existente? <a href="">Clique aqui</a>
+                        Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
                     </p>
                 </div>
             </main>
