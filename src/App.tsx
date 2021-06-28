3e8fbffc56dbb7ef0e2ea76ed4a20520a4550f65
ghblue <ghresilva@.inf.ufpel.edu.br>
2021-06-28 00:49:08 -0300

    server-interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import { AdminRoom } from './pages/AdminRoom';
 import { NewRoom } from "./pages/NewRoom"
 import { Home } from "./pages/Home"
 import { Room } from './pages/Room';
@@ -15,6 +16,8 @@ function App() {
             <Route path="/" exact component={Home}/>
             <Route path="/rooms/new" component={NewRoom}/>
             <Route path="/rooms/:id" component={Room}/>
+
+            <Route path="/admin/rooms/:id" component={AdminRoom}/>
           </Switch>
         </AuthContextProvider>
     </BrowserRouter>
